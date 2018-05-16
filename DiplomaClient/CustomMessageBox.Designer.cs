98700818c3032dba94405a87cd4eb5bc7c56dd8c
smoukiDev <smakfactory@gmail.com>
2018-05-16 03:12:21 +0300

    Add CustomMessageBox Default Icon

--- DiplomaClient/CustomMessageBox.Designer.cs
+++ DiplomaClient/CustomMessageBox.Designer.cs
@@ -168,6 +168,7 @@
             this.Controls.Add(this.pbIcon);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "CustomMessageBox";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CustomMessageBox";
