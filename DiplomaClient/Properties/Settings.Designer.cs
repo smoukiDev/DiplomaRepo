b8da8def3e4ce386a9c3fae2ce8d2cdc5be9d846
smoukiDev <smakfactory@gmail.com>
2018-05-18 02:19:02 +0300

    Revert "Add SalesHistory DataSet"
    This reverts commit 85aea22d16c066959c2d25e169d6667620da350e.

--- DiplomaClient/Properties/Settings.Designer.cs
+++ DiplomaClient/Properties/Settings.Designer.cs
@@ -8,29 +8,22 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace DiplomaClient.Properties {
-    
-    
+namespace DiplomaClient.Properties
+{
+
+
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.5.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
-        
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
+    {
+
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-        
-        public static Settings Default {
-            get {
+
+        public static Settings Default
+        {
+            get
+            {
                 return defaultInstance;
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("DATA SOURCE=localhost:1521/pdbortw;PASSWORD=mining;PERSIST SECURITY INFO=True;USE" +
-            "R ID=SH")]
-        public string SalesHistoryConnectionString {
-            get {
-                return ((string)(this["SalesHistoryConnectionString"]));
             }
         }
     }
